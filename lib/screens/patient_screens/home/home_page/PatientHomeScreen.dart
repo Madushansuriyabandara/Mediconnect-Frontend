55cb4e0bb64d31c165d3ea129bab12fa64a5e769
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-09-05 20:09:46 +0530

    initial commit

--- lib/screens/patient_screens/home/home_page/PatientHomeScreen.dart
+++ lib/screens/patient_screens/home/home_page/PatientHomeScreen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../../../themes/appointmentStatusColors.dart';
 import '../../../../themes/bottomNavBar/patientBottomNavBar.dart';
 import '../../../../themes/switchUser.dart';
+import '../appointment_details/AppointmentDetailsScreen.dart';
 
 class PatientHomeScreen extends StatelessWidget {
   final String userEmail = "johndoe@example.com";
@@ -30,6 +31,104 @@ class PatientHomeScreen extends StatelessWidget {
           children: [
             Center(
               child: Container()
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'My Appointments',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            AppointmentButton(
+              color: getAppointmentStatusColor('Queued'),
+              text: 'John Doe - Chest Pain',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AppointmentDetailsScreen(
+                      appointmentName: 'Chest Pain',
+                      doctorName: 'Dr. John Doe',
+                      specialty: 'Cardiac Surgeon',
+                      appointmentTime: '10:30am - 11:00am',
+                      appointmentDate: 'February 15, 2024',
+                      location: 'Suwa Piyasa - Kurunegala',
+                      appointmentNumber: 34,
+                      currentNumber: 23,
+                      turnTime: '10:43am',
+                      appointmentStatus: 'Queued',
+                    ),
+                  ),
+                );
+              },
+            ),
+            AppointmentButton(
+              color: getAppointmentStatusColor('Upcoming'),
+              text: 'Simon Powel - Leg injury',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AppointmentDetailsScreen(
+                      appointmentName: 'Leg injury',
+                      doctorName: 'Dr. Simon Powel',
+                      specialty: 'Orthopedic Surgeon',
+                      appointmentTime: '11:00am - 11:30am',
+                      appointmentDate: 'February 15, 2024',
+                      location: 'Suwa Piyasa - Kurunegala',
+                      appointmentNumber: 35,
+                      currentNumber: 25,
+                      turnTime: '11:15am',
+                      appointmentStatus: 'Upcoming',
+                    ),
+                  ),
+                );
+              },
+            ),
+            AppointmentButton(
+              color: getAppointmentStatusColor('Missed'),
+              text: 'Eddie Brownrick - Knee pain',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AppointmentDetailsScreen(
+                      appointmentName: 'Knee pain',
+                      doctorName: 'Dr. Eddie Brownrick',
+                      specialty: 'Orthopedic Surgeon',
+                      appointmentTime: '11:30am - 12:00pm',
+                      appointmentDate: 'February 15, 2024',
+                      location: 'Suwa Piyasa - Kurunegala',
+                      appointmentNumber: 36,
+                      currentNumber: 26,
+                      turnTime: '11:45am',
+                      appointmentStatus: 'Missed',
+                    ),
+                  ),
+                );
+              },
+            ),
+            AppointmentButton(
+              color: getAppointmentStatusColor('Completed'),
+              text: 'Richard Ryman - Eye',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AppointmentDetailsScreen(
+                      appointmentName: 'Eye Checkup',
+                      doctorName: 'Dr. Richard Ryman',
+                      specialty: 'Ophthalmologist',
+                      appointmentTime: '12:00pm - 12:30pm',
+                      appointmentDate: 'February 15, 2024',
+                      location: 'Suwa Piyasa - Kurunegala',
+                      appointmentNumber: 37,
+                      currentNumber: 27,
+                      turnTime: '12:15pm',
+                      appointmentStatus: 'Completed',
+                    ),
+                  ),
+                );
+              },
             ),
           ],
         ),
