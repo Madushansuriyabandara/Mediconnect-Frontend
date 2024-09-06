ae3f8b3439949f098fdc1c59cb88fb296e387b87
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-09-06 20:11:13 +0530

    Update appointment_details.dart

--- lib/screens/patient_screens/home/appointment_details/appointment_details.dart
+++ lib/screens/patient_screens/home/appointment_details/appointment_details.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'view/view.dart';
+
+import '../../../../themes/appointmentStatusColors.dart';
+import '../rate/RateScreen.dart';
 
 class AppointmentDetailsScreen extends StatelessWidget {
   final String appointmentName;
@@ -29,17 +31,111 @@ class AppointmentDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppointmentDetailsScaffold(
-      appointmentName: appointmentName,
-      doctorName: doctorName,
-      specialty: specialty,
-      appointmentTime: appointmentTime,
-      appointmentDate: appointmentDate,
-      location: location,
-      appointmentNumber: appointmentNumber,
-      currentNumber: currentNumber,
-      turnTime: turnTime,
-      appointmentStatus: appointmentStatus,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Appointment Details'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              leading: const Icon(Icons.person, size: 40),
+              title: Text(
+                "$doctorName - $appointmentName",
+                style:
+                const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("$doctorName($specialty)"),
+            ),
+            const Divider(height: 30),
+            ListTile(
+              leading: const Icon(Icons.calendar_today, size: 40),
+              title: Text(
+                appointmentTime,
+                style: const TextStyle(fontSize: 18),
+              ),
+              subtitle: Text(appointmentDate),
+            ),
+            ListTile(
+              leading: const Icon(Icons.location_on, size: 40),
+              title: Text(location),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "My number: $appointmentNumber",
+              style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Current attending number: $currentNumber",
+              style: const TextStyle(fontSize: 18),
+            ),
+            Text(
+              "My turn at (approx.): $turnTime",
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              width: double.infinity,
+              color: getAppointmentStatusColor(
+                  appointmentStatus), // Use the status color method
+              child: Center(
+                child: Text(
+                  "Appointment Status: $appointmentStatus",
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RateScreen(
+                          doctorName: 'Dr. John Doe',
+                        ),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 10),
+                  ),
+                  child: const Text(
+                    "Rate",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle remove appointment action
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 10),
+                  ),
+                  child: const Text(
+                    "Remove Appointment",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
