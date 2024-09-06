0d7a74da9b978ee6c37020c037ad3894396d57aa
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-09-06 20:21:06 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mediconnect',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const GetStartedScreen()
+        title: 'Mediconnect',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const GetStartedScreen()
     );
   }
 
